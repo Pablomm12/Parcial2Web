92dc567349f20d9007b6c8df053e166ba68f58c6
Pablomm12 <98631018+Pablomm12@users.noreply.github.com>
2024-11-27 23:45:51 -0500

    third

---
+++ src/shared/testing-utils/typeorm-testing-config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* archivo src/shared/testing-utils/typeorm-testing-config.ts*/
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+export const TypeOrmTestingConfig = () => [
+ TypeOrmModule.forRoot({
+   type: 'sqlite',
+   database: ':memory:',
+   dropSchema: true,
+   entities: [],
+   synchronize: true,
+   keepConnectionAlive: true
+ }),
+ TypeOrmModule.forFeature([]),
+];
+/* archivo src/shared/testing-utils/typeorm-testing-config.ts*/
